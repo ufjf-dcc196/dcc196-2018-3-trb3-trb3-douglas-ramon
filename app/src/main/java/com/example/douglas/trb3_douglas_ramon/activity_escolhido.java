f6a59c3e756b658f4d5915523d9e539617ecf16d
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-11-30 17:09:21 -0200

    Activity detalhe livro para reserva

---
+++ app/src/main/java/com/example/douglas/trb3_douglas_ramon/activity_escolhido.java
@@ -0,0 +1,13 @@
+package com.example.douglas.trb3_douglas_ramon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_escolhido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolhido);
+    }
+}
